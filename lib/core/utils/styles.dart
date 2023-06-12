53c6ce0310b9ecc9609bee4df66ea23988750daa
YousabSefeen <jesusyousab@gmail.com>
2023-06-12 04:28:23 +0200

    Create app styles

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 
 abstract class Styles {
-  static const titleMedia = TextStyle(
-    fontSize: 20,
+  static const kGTSectraFine = 'GT Sectra Fine';
+  static const textStyle18 = TextStyle(
+    fontSize: 18,
     fontWeight: FontWeight.w600,
+  );
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400, //regular
+  );
+  static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500, //regular
+  );
+  static const textStyle20a = TextStyle(
+      fontSize: 20, fontWeight: FontWeight.w400, fontFamily: kGTSectraFine);
+  static const textStyle20b = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    //fontFamily  by default
+  );
+  static const textStyle30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w400,
+    fontFamily: kGTSectraFine,
   );
 }
--- lib/features/home/presentation/views/screens/home_screen.dart
+++ lib/features/home/presentation/views/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../core/utils/assets_data.dart';
 import '../../../../../core/utils/styles.dart';
 import '../widgets/custom_list_view_images.dart';
 
@@ -13,15 +14,60 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: deviceSize.width * 0.05),
-        child: Column(
+        child: const Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
+          children: [
             CustomAppBar(),
             CustomListViewImages(),
             SizedBox(height: 45),
-            Text('Best Seller', style: Styles.titleMedia)
+            Text('Best Seller', style: Styles.textStyle18),
+            BestSellerListView()
           ],
         ),
+      ),
+    );
+  }
+}
+
+class BestSellerListView extends StatelessWidget {
+  const BestSellerListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size deviceSize = MediaQuery.of(context).size;
+    return Container(
+      color: Colors.red,
+      width: deviceSize.width * 0.9,
+      height: deviceSize.height * 0.17,
+      child: Row(
+        children: [
+          SizedBox(
+            child: AspectRatio(
+              aspectRatio: 2 / 3,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  image: const DecorationImage(
+                    image: AssetImage(AssetsData.kMessi),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+            child: Column(
+              children: [
+                // Text(
+                //   'More than a convening of big thinkers and chance meetings, the Aspen Ideas Festiva',
+                //   maxLines: 2,
+                //   overflow: TextOverflow.ellipsis,
+                // )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
